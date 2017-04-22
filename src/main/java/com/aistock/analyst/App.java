333e9c0a9e095bf758956a43af639a439b922970
sclin0323 <sclin0323@gmail.com>
2017-04-22 22:44:46 +0800

    first commit

---
+++ src/main/java/com/aistock/analyst/App.java
@@ -0,0 +1,13 @@
+package com.aistock.analyst;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

55315365500678bcfd9aa5eb817a415fc7284fa3
sclin0323 <sclin0323@gmail.com>
2017-04-27 23:20:34 +0800

    update

--- src/main/java/com/aistock/analyst/repository/DailyStockRepository.java
+++ src/main/java/com/aistock/analyst/repository/DailyStockRepository.java
@@ -3,6 +3,7 @@ package com.aistock.analyst.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.aistock.analyst.entity.DailyStock;
@@ -16,6 +17,8 @@ public interface DailyStockRepository extends MongoRepository<DailyStock, String
 	
 	public List<DailyStock> findByDateAndDifStatus(String date, String status);
 	
+	@Query(value = "{}", fields = "{'jarFileBytes': 0}")
+	public List<DailyStock> findField();
 	//List<DailyStock> findByDateContaining(String date);
 }
 
